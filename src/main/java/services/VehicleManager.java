d2336d318b95454c4a2e68ceba66b648c3c152de
Johnny Ilanga <ilangajohnny18@gmail.com>
2025-02-14 23:50:41 +0200

    started working on the vehicleManger class which will contain our business logic

--- src/main/java/services/VehicleManager.java
+++ src/main/java/services/VehicleManager.java
@@ -1,4 +1,67 @@
 package services;
 
+import factory.VehicleFactory;
+import models.Vehicle;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class VehicleManager {
+    private List<Vehicle> vehicles;
+    private final VehicleFactory vehicleFactory = new VehicleFactory();
+
+    public VehicleManager(){
+        this.vehicles = new ArrayList<>();
+    }
+
+    public Vehicle createVehicle(String vehicle){
+       Vehicle newVehicle = vehicleFactory.getInstanceOf(vehicle);
+       vehicles.add(newVehicle);
+       return newVehicle;
+    }
+
+    public List<Vehicle> getAllVehicles(){
+        return vehicles;
+    }
+
+    public List<Vehicle> getVehicleByType(String vehicleT) {
+        return vehicles.stream().filter(
+                vehicle -> vehicle.vehicleType().equalsIgnoreCase(vehicleT)).toList();
+    }
+
+    public List<Vehicle> getVehicleByFuelType(String fuelT){
+        return vehicles.stream().filter(vehicle -> vehicle.fuelType().toLowerCase().contains(fuelT)).toList();
+    }
+
+    public Vehicle getVehicleByName(String name){
+        Optional<Vehicle> vehicleOptional = vehicles.stream()
+                .filter(vehicle1 -> vehicle1.fuelType().equalsIgnoreCase(name)).findFirst();
+        return vehicleOptional.orElse(null);
+    }
+
+    public void deleteVehicle(){
+
+    }
+
+    public void deleteVehicleByType(){
+
+    }
+
+    public void deleteVehicleByFuelType(){
+
+    }
+
+    public void deleteVehicleByAbsoluteFuelType(){
+
+    }
+
+    public void deleteVehicleByName(){
+
+    }
+
+    public void saveVehicles(){
+        // w shall write to our json here
+    }
+
 }
--- src/main/java/ui/MainMenu.java
+++ src/main/java/ui/MainMenu.java
@@ -1,4 +1,56 @@
 package ui;
 
+import models.Vehicle;
+import services.VehicleManager;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+
 public class MainMenu {
+    private static final BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    private static final List<String> menuChoices = List.of("1","2","3","4","5");
+    private static final VehicleManager vehicleManager = new VehicleManager();
+
+    public static void main(String[] args) {
+        play();
+    }
+
+
+    public static void play(){
+        menu();
+        String choice = prompt(
+                "Enter your choice: ",
+                "\nIncorrect Selection, select 1-5", menuChoices);
+
+    }
+
+    public static void menu(){
+        String menuChoice = """
+                Welcome to the Vehicle Management System!
+                
+                1. Create a new vehicle
+                2. View a vehicle's details
+                3. List all vehicles
+                4. Help
+                5. Exit
+                """;
+
+        System.out.println(menuChoice);
+    }
+
+    public static String prompt(String promptMsg, String incorrectSelectionMessage,  List<String> choices) {
+        while (true){
+            try {
+                System.out.print(promptMsg);
+                String choice = input.readLine();
+                if (choices.contains(choice)) return choice;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(incorrectSelectionMessage);
+        }
+    };
 }
